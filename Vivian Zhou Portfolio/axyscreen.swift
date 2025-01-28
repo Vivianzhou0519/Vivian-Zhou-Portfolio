bfe3f4526ce76a655b86427e6a16ac5ac682cb10
Vivian Zhou <vivianzhou0519@gmail.com>
2025-01-28 00:17:39 -0800

    switches to screens

---
+++ Vivian Zhou Portfolio/axyscreen.swift
@@ -0,0 +1,18 @@
+//
+//  axyscreen.swift
+//  Vivian Zhou Portfolio
+//
+//  Created by Vivian Zhou on 1/27/25.
+//
+
+import SwiftUI
+
+struct axyscreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    axyscreen()
+}
